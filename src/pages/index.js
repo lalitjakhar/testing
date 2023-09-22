dd27eb31d4c6db0052917c5c16228a963d7e23ef
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-09-22 18:41:56 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,21 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <h1>hello</h1>
+      <section className={styles.main_navbar_content}>
+        <div className="container-fluid d-flex">
+        <h1>hello</h1>
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
