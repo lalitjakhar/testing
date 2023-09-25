3a6caa84962090352952b3a6178c68e1058c367f
lalitjakhar <lalitkumarjakhar22216@gmail.com>
2023-09-25 12:39:20 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -105,6 +105,24 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section className={styles.sec_start_body}>
+        <div className={styles.sec_start_body_content}>
+          <h1>Quick Start</h1>
+          <h5>Welcome to the React documentation! This page will give you an introduction to the 80% of React concepts that you will use on a daily basis.</h5>
+          <div className={styles.sec_start_body_content_box}>
+            <h3>You will learn</h3>
+            <hr />
+            <ul>
+              <li>How to create and nest components</li>
+              <li>How to add markup and styles</li>
+              <li>How to display data</li>
+              <li>How to render conditions and lists</li>
+              <li>How to respond to events and update the screen</li>
+              <li>How to share data between components</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
